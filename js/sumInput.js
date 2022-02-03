6301962bd4761f3d1580890e3bab037f84a6e29a
sunnyfur <polin.makarova@mail.ru>
2022-02-03 08:56:39 +0500

    change for to while

--- js/sumInput.js
+++ js/sumInput.js
@@ -1,12 +1,22 @@
 const sumInput = () => {
     let arr = [];
-    sum = 0;
-    for (;;) {
-        let value = prompt("Введите число", '');
-        if (value === '' || (!(+value) && value !== '0')) break;
-        arr.push(+value);
+    let sum = 0;
 
+
+    let condition = false;
+    do {
+        let value = prompt("Введите число", '');
+        condition = !(value === '' || (!(+value) && value !== '0'));
+        if (condition) arr.push(+value);
     }
+    while (condition);
+
+    // for (;;) {
+    //     let value = prompt("Введите число", '');
+    //     if (value === '' || (!(+value) && value !== '0')) break;
+    //     arr.push(+value);
+
+    // }
     alert(arr.sort((a, b) => a - b));
     arr.forEach((elem) => {
         sum += elem;
